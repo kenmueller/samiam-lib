5991d1264f8cc85910dd0caa0148ec9fdbdc94bd
Ken Mueller <kenmueller0@gmail.com>
2023-06-12 14:08:30 -0700

    Fixed adding node to not return set

--- src/belief-network.ts
+++ src/belief-network.ts
@@ -3,5 +3,7 @@ import Node from './node'
 export default class BeliefNetwork {
 	nodes = new Set<Node>()
 
-	addNode = (node: Node) => this.nodes.add(node)
+	addNode = (node: Node) => {
+		this.nodes.add(node)
+	}
 }
