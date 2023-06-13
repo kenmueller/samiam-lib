a9ab154a9184e57e7f9fd7cfdcaaa36b123cc1f1
Scott Mueller <scott@cs.ucla.edu>
2023-06-13 14:42:00 -0700

    can remove node values, but not yet tested

--- src/belief-network.ts
+++ src/belief-network.ts
@@ -2,8 +2,10 @@ import Node from './node'
 
 export default class BeliefNetwork {
 	nodes = new Set<Node>()
+	nodeNames = new Set<string>()
 
-	addNode = (node: Node) => {
-		this.nodes.add(node)
-	}
+	// addNode = (node: Node) => {
+	// 	this.nodes.add(node)
+	// 	this.nodeNames.add(node.name)
+	// }
 }
