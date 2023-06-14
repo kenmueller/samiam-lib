c97b81264fcee5fbeeafbb985cbadf9d7db300ba
Scott Mueller <scott@cs.ucla.edu>
2023-06-14 03:46:40 -0700

    Adding and removing values and parents, with tests

--- src/util.ts
+++ src/util.ts
@@ -3,3 +3,10 @@ export const isSubset = <T>(subset: T[], superset: T[]): boolean =>
 
 export const pluck = <T, K extends keyof T>(objects: T[], key: K): T[K][] =>
 	objects.map(object => object[key])
+
+export const equalArrays = <T>(a: T[], b: T[]) =>
+	a.length === b.length && a.every((element, index) => element === b[index])
+
+export const equal2DArrays = <T>(a: T[][], b: T[][]) =>
+	a.length === b.length &&
+	a.every((element, index) => equalArrays(element, b[index]))
