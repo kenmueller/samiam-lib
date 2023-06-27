8b4b0e3f7728ffb2ed2f9bc6ea3b52da82105630
Scott Mueller <scott@cs.ucla.edu>
2023-06-27 13:48:00 -0700

    Dummy probability of evidence function

--- src/belief-network.ts
+++ src/belief-network.ts
@@ -1,4 +1,5 @@
 import Node from './node'
+import Observation from './observation'
 
 export default class BeliefNetwork<NodeLike extends Node = Node> {
 	nodes = new Set<NodeLike>()
@@ -7,5 +8,9 @@ export default class BeliefNetwork<NodeLike extends Node = Node> {
 	addNode = (node: NodeLike) => {
 		this.nodes.add(node)
 		// this.nodeNames.add(node.name)
+	}
+
+	probability = (evidence: Set<Observation>) => {
+		return 0.5
 	}
 }
